29fe49ff7a2b144269166b9afa2bf2f8420785fd
ArnT81 <anders_soderberg@hotmail.com>
2023-11-14 08:38:15 +0100

    Added City model, CityQuery and ignored Secret

---
+++ Query with Dapper/DataAccess/CityQuery.cs
@@ -0,0 +1,15 @@
+﻿namespace Query_with_Dapper.DataAccess
+{
+    public class CityQuery
+    {
+        public string ConnectionString { get; set; }
+        public CityQuery()
+        {
+            string password = Secret.DbPassword();
+            ConnectionString = $"server=localhost;database=world;password={password};user=root;AllowUserVariables=true\\";
+        }
+
+
+
+    }
+}
---
+++ Query with Dapper/Models/City.cs
@@ -0,0 +1,9 @@
+﻿namespace Query_with_Dapper.Models
+{
+    public class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int population { get; set; }
+    }
+}
