2eda0dc0a4f98e1aee659ee982c99b205b39b5d3
ArnT81 <anders_soderberg@hotmail.com>
2023-11-17 09:17:26 +0100

    Added description to GUI

--- Query with Dapper/wwwroot/js/home.js
+++ Query with Dapper/wwwroot/js/home.js
@@ -1,4 +1,8 @@
-﻿const clearContent = () => $('.content').empty();
+﻿const clearContent = () => {
+    $('.content').empty();
+    $('.description').empty();
+
+}
 
 
 const getData = async (string) => {
@@ -9,6 +13,19 @@ const getData = async (string) => {
     const data = await response;
 
     return data;
+};
+
+
+const handleDescription = (assignment) => {
+    return {
+        0: "YEHHO",
+        1: `Present a list of strongly typed city data for all cities in a span between two population numbers. Use a view file (cshtml) for the presentation and use a table for the data.`,
+        3: `Limit the number of posts read.`,
+        2: `Read all cities by country code.`,
+        4: `Read all European countries ordered by life expectancy, high to low.`,
+        5: `Read all cities in a country.`,
+        6: `Read all cities in a continent with a life expectancy above a certain age.`,
+    }[assignment] || notFound();
 };
 
 
@@ -26,13 +43,17 @@ const handleResponse = (assignment) => {
 
 
 const renderData = (data) => {
-    clearContent()
-
     console.log('get Assignment text', data)
     data?.forEach((item) => {
         $('.content').append(`<div> ${item.name}</div>`)
     })
 };
+
+
+const renderDescription = (description) => {
+    $('.description').html(`<div><p>${description}</p></div>`)
+};
+
 
 
 $(() => {
@@ -41,6 +62,10 @@ $(() => {
     //EVENTLISTENERS
     $('#select_category').on('change', async function () {
         const data = await handleResponse($(this).val());
+        const description = await handleDescription($(this).val());
+
+        clearContent()
         renderData(data)
+        renderDescription(description)
     })
 })
